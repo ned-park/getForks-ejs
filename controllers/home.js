dfdae5398687267984eb0217b251c79975f35bea
ned-park <87483870+ned-park@users.noreply.github.com>
2022-09-12 22:44:26 -0400

    Updates homepage to display recipes and descriptions

--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,18 @@
 const User = require('../models/User')
+const Repo = require('../models/Repo')
 
 module.exports = {
     getIndex: async (req,res) => {
+      try {
+        const repos = await Repo.find({}).populate('userId')
         if (req.user) {
             const user = await User.find({_id: req.user.id})
-            res.render('index.ejs', {user: req.user}) 
+            res.render('index.ejs', {user: req.user, repos: repos}) 
           } else {
-            res.render('index.ejs', {user: null})
+            res.render('index.ejs', {user: null, repos: repos})
           }
+        } catch(err) {
+          console.log(err)
+        }
     }
 }
