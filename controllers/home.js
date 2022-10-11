2ded395207cb306977f9adf8bb6053b644e9c09b
ned-park <87483870+ned-park@users.noreply.github.com>
2022-10-11 13:15:30 -0400

    Adds pagination to homepage

--- controllers/home.js
+++ controllers/home.js
@@ -3,13 +3,16 @@ const Repo = require('../models/Repo')
 
 module.exports = {
     getIndex: async (req,res) => {
+      console.log(req.query)
+      const { page, limit} = { page: +req.query.page || 1, limit: +req.query.limit || 5};
+      console.log(page, limit)
       try {
-        const repos = await (await Repo.find({}).sort({creationDate: -1}).populate('userId')).filter(repo => !repo.forkedFrom)
+        const repos = await (await Repo.find({ forkedFrom: { $exists: false } } ) .limit(limit * 1).skip((page - 1) * limit).sort({creationDate: -1}).populate('userId')).filter(repo => !repo.forkedFrom)
         if (req.user) {
             const user = await User.find({_id: req.user.id})
-            res.render('index.ejs', {user: req.user, repos: repos}) 
+            res.render('index.ejs', {user: req.user, repos: repos, page: page, limit: limit}) 
           } else {
-            res.render('index.ejs', {user: null, repos: repos})
+            res.render('index.ejs', {user: null, repos: repos, page: page, limit: limit})
           }
         } catch(err) {
           console.log(err)
