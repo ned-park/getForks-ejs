41ea33eeef08d858e98c012916c7e18b1403efe4
ned-park <87483870+ned-park@users.noreply.github.com>
2022-10-18 15:52:22 -0400

    Updates search by tags

--- controllers/search.js
+++ controllers/search.js
@@ -6,9 +6,11 @@ module.exports = {
       try {
         const { page, limit} = { page: +req.query.page || 1, limit: +req.query.limit || 5};
         console.log(page, limit)
-        const repos = await Repo.find({
-          title: {$regex: req.query.query, $options: 'i'},  
-          }).limit(limit * 1).skip((page - 1) * limit).sort({creationDate: -1}).populate('userId').lean()
+        const repos = await Repo.find({$or: [{
+            title: {$regex: req.query.query, $options: 'i'}}, 
+            {tags: {$regex: req.query.query, $options: 'i'}}
+          ]
+        }).limit(limit * 1).skip((page - 1) * limit).sort({creationDate: -1}).populate('userId').lean()
         if (req.user) {
             const user = await User.find({_id: req.user.id})
             res.render('index.ejs', {user: req.user, repos: repos, page: page, limit: limit}) 
